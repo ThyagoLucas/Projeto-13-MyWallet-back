06a6818ea61bce6579c943ad1f3dc7368ac492c7
Thyago Lucas <Salc@icomp.ufam.edu.br>
2022-05-06 23:40:42 -0400

    Commit com o servidor rodando e com conexão com o bando de dados remoto no Atlas

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-import { Express } from "express";
+import express from "express";
+import cors from 'cors';
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from 'dotenv';
+
+
+const app = express();
+dotenv.config();
+
+app.listen(5000, ()=>{
+    console.log("Servidor rodando na porta 5000");
+});
+app.use(cors());
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+
+let db = mongoClient.db(`${process.env.DATA_BASE}`);
+
+mongoClient.connect();
+
+
+
+
+
 
