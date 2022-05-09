6352d7c03d9fa9339f63b33d4ede2885e52b5495
Thyago Lucas <Salc@icomp.ufam.edu.br>
2022-05-09 05:44:58 -0400

    make it better

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-import express from "express";
+import express, { json } from "express";
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import loginAndRegisterRouter from "./routers/loginAndRegisterRouter.js";
 import infoAccountRouter from "./routers/infoAccountRouter.js";
 import transaction from "./routers/addTransactionRouter.js";
 
+dotenv.config();
+
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(json());
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
