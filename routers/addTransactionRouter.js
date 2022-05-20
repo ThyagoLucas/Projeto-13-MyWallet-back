04e36bdff964420fc5276d80dae1ec262950f7f4
Matheus Galdino <matheus.kho@gmail.com>
2022-05-20 18:25:20 -0300

    Preparando revisão de código

--- routers/addTransactionRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { postTransaction } from "../controllers/accountMovimentationController.js";
-import { validDatasTransaction } from "../middlewares/validTransactionMiddlerware.js";
-
-const transaction = Router();
-
-transaction.post('/movimentation', validDatasTransaction, postTransaction);
-
-export default transaction;
